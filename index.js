7bffda130b55a14a2f281dea044513d391c81877
Adrian Bielinski <Adrian.Bielinski@training.futuretrainings.com>
2020-02-13 14:09:03 +0100

    change

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Server is running");
+  console.log("Server is running " + port);
 });
